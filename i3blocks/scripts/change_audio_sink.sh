f2343e566a4d8f59fc6b1b2d4f88ce16ea7b6c7c
Rolan <55665063+Erasial@users.noreply.github.com>
2025-02-28 19:44:27 +0200

    Delete i3blocks directory

--- i3blocks/scripts/change_audio_sink.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SINK="$(pactl get-default-sink)"
-
-if [ "$SINK" = "alsa_output.pci-0000_00_1b.0.analog-stereo" ]; then
-	pacmd set-default-sink alsa_output.usb-PreSonus_AudioBox_USB_96_000000000000-00.analog-stereo
-else
-	pacmd set-default-sink alsa_output.pci-0000_00_1b.0.analog-stereo
-fi
